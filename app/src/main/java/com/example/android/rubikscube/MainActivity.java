2af5407df573b5d46ab8eac2085f1d73cb0d2c73
Laine Otto <laine@kaigen.us>
2019-07-13 22:18:23 -0700

    Added tables

--- app/src/main/java/com/example/android/rubikscube/MainActivity.java
+++ app/src/main/java/com/example/android/rubikscube/MainActivity.java
@@ -5,9 +5,20 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int[] whiteArr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int[] blueArr = {10, 11, 12, 13, 14, 15, 16, 17, 18};
+    int[] orangeArr = {19, 20, 21, 22, 23, 24, 25, 26, 27};
+    int[] greenArr = {28, 29, 30, 31, 32, 33, 34, 35, 36};
+    int[] redArr = {37, 38, 39, 40, 41, 42, 43, 44, 45};
+    int[] yellowArr = {46, 47, 48, 49, 50, 51, 52, 53, 54};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void up(int face) {
+
     }
 }
